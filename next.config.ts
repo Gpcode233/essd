fd2121b057b393a4562ff19c660528e36e480062
Gpcode233 <godspowerojini8@gmail.com>
2026-09-09 17:20:44 +0100

    fix(vercel): add prisma generate and db push to build and postinstall scripts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  outputFileTracingRoot: path.resolve(__dirname),
 };
 
 export default nextConfig;
